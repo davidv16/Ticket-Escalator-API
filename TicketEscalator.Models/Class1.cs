13b6bae610e1c9b268161a8ed3c4663163ee5c9a
davidv16 <dabbithekking@gmail.com>
2021-09-25 20:40:55 +0000

    setting up api project and connecting projects

---
+++ TicketEscalator.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TicketEscalator.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ TicketEscalator.Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TicketEscalator.Repositories
+{
+    public class Class1
+    {
+    }
+}
